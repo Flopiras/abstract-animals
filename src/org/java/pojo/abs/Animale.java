9e7848423e16df8a3f125a84c2ec2ed2f6e919fb
Flopiras <Flopiras@yahoo.it>
2023-11-16 12:49:56 +0100

    added Animal class

---
+++ src/org/java/pojo/abs/Animale.java
@@ -0,0 +1,43 @@
+package org.java.pojo.abs;
+
+public abstract class Animale {
+	
+	private String nome;
+	private int zampe;
+	
+	//constructor
+	public Animale(String nome, int zampe) {
+		
+		setNome(nome);
+		setZampe(zampe);
+		
+	}
+
+	//getters and setters
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getZampe() {
+		return zampe;
+	}
+
+	public void setZampe(int zampe) {
+		this.zampe = zampe;
+	}
+
+	//abstract methods
+	public abstract String dormi();
+	
+	//override
+	@Override
+	public String toString() {
+
+		return "Mi chiamo " + getNome() + "\n"
+			+ "e ho " + getZampe() + " zampe";
+	}
+}
